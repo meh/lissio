aa40a53a3c62e031a7f050034af023ed5a533525
meh <meh@schizofreni.co>
2013-07-29 18:12:05 +0200

    view: add some more stuff

--- opal/lissio/view.rb
+++ opal/lissio/view.rb
@@ -14,6 +14,77 @@ class View
 	def self.element(name = nil)
 		name ? @element = name : @element
 	end
+
+	def self.tag_name(name = nil)
+		name ? @tag_name = name : @tag_name
+	end
+
+	def self.class_name(name = nil)
+		name ? @class_name = name : @class_name
+	end
+
+	def self.events
+		@events ||= Hash.new { |h| h[k] = [] }
+	end
+
+	def self.on(name, selector = nil, &block)
+		events[name] << [selector, block]
+	end
+
+	def self.render(&block)
+		define_method :render do |*args|
+			instance_exec(*args, &block)
+		end
+	end
+
+	attr_accessor :parent
+
+	def tag_name
+		self.class.tag_name || :div
+	end
+
+	def class_name
+		self.class.class_name || ""
+	end
+
+	def element
+		return @element if @element
+
+		scope = parent ? parent.element : $document
+		elem  = if elem = self.class.element
+			scope.at(elem)
+		else
+			Element.create tag_name
+		end
+
+		elem.add_class class_name unless class_name.empty?
+
+		events.each {|name, blocks|
+			blocks.each {|selector, block|
+				elem.on(name, &block)
+			}
+		}
+
+		@element = elem
+	end
+
+	def render(*); end
+
+	def remove
+		@element.remove if @element
+	end
+
+	def destroy
+		return unless @element
+
+		events.each {|name, blocks|
+			blocks.each {|selector, block|
+				@element.off(block)
+			}
+		}
+
+		@element.remove
+	end
 end
 
 end

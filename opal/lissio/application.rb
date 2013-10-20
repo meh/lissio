6c7279d11912263eca666c6f022f4f05a9fb3615
meh <meh@schizofreni.co>
2013-10-20 22:35:34 +0200

    application: some improvements

--- opal/lissio/application.rb
+++ opal/lissio/application.rb
@@ -4,13 +4,37 @@ require 'forwardable'
 module Lissio
 
 class Application < Component
-	include Singleton
+	def self.inherited(klass)
+		super
 
-	extend SingleForwardable
+		klass.include Singleton
+
+		$document.on :load do
+			klass.start
+		end
+	end
+
+	def self.expose(what, options = {})
+		if what.start_with?(?@)
+			name = what[1 .. -1]
+
+			define_singleton_method name do
+				instance.__send__ name
+			end
+
+			attr_reader name
+		else
+			define_singleton_method what do |*args, &block|
+				instance.__send__ what, *args, &block
+			end
+		end
+	end
+
+	expose :start
+	expose :navigate
+
 	extend Forwardable
-
-	def_single_delegators :instance, :start, :navigate
-	def_instance_delegators :@router, :navigate
+	def_delegators :@router, :navigate, :route
 
 	def initialize
 		@router = Lissio::Router.new(fragment: false)
